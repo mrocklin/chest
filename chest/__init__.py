fe8bc297e45a442c39fe8c5e45d032276a07f412
Matthew Rocklin <mrocklin@gmail.com>
2014-11-29 09:21:27 -0800

    add setup.py set version to 0.1.0

--- chest/__init__.py
+++ chest/__init__.py
@@ -1,1 +1,3 @@
 from .core import Chest
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+import chest
+
+setup(name='chest',
+      version=chest.__version__,
+      description='Simple on-disk dictionary',
+      url='http://github.com/mrocklin/chest/',
+      author='https://raw.github.com/mrocklin/chest/master/AUTHORS.md',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='dictionary out-of-core',
+      packages=['chest'],
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      zip_safe=False)
